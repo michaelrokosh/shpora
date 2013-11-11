c1f207d44a7e5f17bc84177b4da50030b28b9c7e
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-11 06:56:13 -0500

    Added some stuff on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
 	def index
+		@recent_pages = Page.order("created_at desc").limit(4).offset(0)
+		if user_signed_in?
+			@recent_user_pages = current_user.pages.order("created_at desc").limit(4).offset(0)
+		end
 	end
 end
