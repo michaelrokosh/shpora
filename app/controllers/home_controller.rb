03c76c707295cc22a3106eb778572b4a3394421f
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-11 14:25:12 -0500

    Some fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@recent_pages = Page.order("created_at desc").limit(4).offset(0)
+		@recent_pages = Page.order("created_at desc").limit(5).offset(0)
 		if user_signed_in?
 			@recent_user_pages = current_user.pages.order("created_at desc").limit(4).offset(0)
 		end
