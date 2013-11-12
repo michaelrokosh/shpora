a1518193eff0fd9cc381ad2d4114c580196ac8e1
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-12 16:20:59 -0500

    Fixed manifest file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Shpora::Application.configure do
+
+  config.assets.precompile += %w( editor.js )
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
