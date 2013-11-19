e83096d96b2796a8b512c46fef4cf36e558da725
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-19 10:20:10 -0500

    New rellic 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save { self.username = username.downcase }
+  before_save { self.email = email.downcase }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :favorites
