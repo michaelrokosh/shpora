5e47a45672e446cd63f3beb854f04f745d6167af
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-23 07:58:46 -0500

    Shares 1.0

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
+  acts_as_taggable
   has_many :favorites
   belongs_to :user
   has_many :favoriters, through: :favorites
@@ -8,8 +9,17 @@ class Page < ActiveRecord::Base
   			format: { with: VALID_URL_REGEX }
   validates :title, presence: true
   validates :content, presence: true
-  validates :tag_list, presence: true
-  acts_as_taggable
+  validates :tag_list, presence: false
+  #validates :tag, length: { minimum: 3, maximum: 15, message: "должнен быть в пределе 3-15 символов" }
+  validate :validate_tag
+
+  def validate_tag
+    tag_list.each do |tag|
+      # This will only accept two character alphanumeric entry such as A1, B2, C3. The alpha character has to precede the numeric.
+      errors.add(:tag, "должнен быть в пределе 3-15 символов") if tag.length > 15 || tag.length < 3
+    end
+  end
+  
   def to_param  # overridden
     url
   end
