3c6d29ffe8622bf063e84f8d822e44e9e25cf43a
Petro <petro.yakubiv@gmail.com>
2017-04-16 09:23:38 +0300

    Add blank slate to show page, until content is setting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def on_the_edit_page?
 		controller_name == 'pages' && action_name == 'new' || action_name == 'edit'
 	end
+
+	def show_page_content(content)
+		if content.blank?
+			render 'content_processing'
+		else
+			sanitize content
+		end
+	end
 end
