15c94fa82f00a8dc485d9859cfddb82c9b216b17
Petro <petro.yakubiv@gmail.com>
2017-04-21 19:13:45 +0300

    Fix url on m subdomain

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user)
+          .permit(:username, :email)
   end
 end
