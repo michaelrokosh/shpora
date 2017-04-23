e1cf780475cfcb0fd15953af2b2a51c44e8858c8
Michael Rokosh <mike@MacBook.local>
2017-04-23 12:37:11 +0300

    Commented out SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,9 @@ Shpora::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
 
-  # config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
+  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
