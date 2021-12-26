473d441a980c43dcf918847dd6426d822e8fda02
Petro <petro.yakubiv@gmail.com>
2021-12-26 12:12:49 +0100

    f

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,11 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
-    if simple_captcha_valid?
+    # if simple_captcha_valid?
       super
-    else
-      flash[:error] = I18n.t('simple_captcha.message.default')
-      redirect_to request.referrer
-    end
+    # else
+    #   flash[:error] = I18n.t('simple_captcha.message.default')
+    #   redirect_to request.referrer
+    # end
   end
 end
